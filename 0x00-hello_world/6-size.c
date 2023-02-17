7672a8c35437759b5469514fea72ee99872c714d
KayalDuncan <duncan.abbirore@gmail.com>
2023-02-16 20:18:13 -0800

    added new scripts and c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *main - a program that print the computer it can be run and compiled on
+ *Return (0) : success
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
+}
