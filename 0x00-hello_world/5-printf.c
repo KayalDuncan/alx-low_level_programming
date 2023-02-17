431806d7e5666c048c191e651b801269309c05cf
KayalDuncan <duncan.abbirore@gmail.com>
2023-02-16 21:10:42 -0800

    updated c files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-  *main- a program that prints a line using a printf function
-  *Return : 0 (Sucess)
+  * main- a program that prints a line using a printf function
+  * Return : 0 (Sucess)
 */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
