fe6ac6ef5dd7ff944ba3c7f02a4edc21c5711ede
KayalDuncan <duncan.abbirore@gmail.com>
2023-02-28 17:48:43 -0800

    add new files on arrays , pointer and strings

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[5];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+
